9ab4e62a1510cfa13e5509d0c5a45a09fa01a03f
Vighnesh Prabhu <57503827+Kratosmsn11@users.noreply.github.com>
2023-07-06 17:05:52 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello LeetCoders!</h1>
+      <h2>These are the starter problems.</h2>
+      <body>
+        <ul>
+          <li>
+            <a href="">Website</a>
+          </li>
+          <li>
+            <a href="mailto:m.bluth@example.com">Email</a>
+          </li>
+          <li>
+            <a href="tel:+123456789">Phone</a>
+          </li>
+        </ul>
+      </body>
+      <div></div>
+    </div>
+  );
+}
